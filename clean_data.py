5c08eb5abe5db5cd833665161933935aeaf37d80
eureyuri <eureynoguchi@gmail.com>
2019-03-29 16:51:45 -0400

    Clean data. Added moving average

--- clean_data.py
+++ clean_data.py
@@ -8,9 +8,8 @@ df = pd.read_csv('training_project2.csv')
 df = df.drop(columns=['open', 'high', 'low', 'close', 'volume', 'dividend', 'split'])
 
 # Split data by IDs and write to different files
-unique_ids = df.id.unique()
-for id in unique_ids:
-    df_new = "df" + str(id)
-    exec('{} = df[df["id"] == id]'.format(df_new))
-    df_new.to_csv(str(id) + ".csv", sep='\t')
-    # df_new['moving_average'] = df['adjusted_close'].rolling(5).mean()
+dfs = dict(tuple(df.groupby('id')))
+list_df = [dfs[x] for x in dfs]
+for index, df in enumerate(list_df):
+    df['moving_average'] = df['adjusted_close'].rolling(5).mean()
+    df.to_csv("data_by_id/" + str(index) + ".csv")
